f1c174ff0d9b89e4f96da15b9b42ad8c4e8bf03f
obe711 <obe711@gmail.com>
2020-05-19 22:45:35 -0400

    OS storage and deployment server

---
+++ PXE-Server.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+#
+# -------------------------------------------------------------------------------------
+# PXE-Server automated installation script for CentOs 7 64bit
+# -------------------------------------------------------------------------------------
+# This script is only intended as a quickstart to test and get familiar with PXE-Server.
+# The HOW-TO should be ALWAYS followed for a fully controlled, manual installation!
+# -------------------------------------------------------------------------------------
+#
+#
+# -------------------------------------------------
+
+
+
+-------------------- TFTP SERVER ------------------
+
+### TFTP SERVER
+
+ln -s '/usr/lib/systemd/system/tftp.socket' '/etc/systemd/system/sockets.target.wants/tftp.socket'
+cd /var/lib/tftpboot
+yes |cp -fra /usr/src/PXE-Server-Centos-7/isolinux/* /var/lib/tftpboot/
+cp /usr/lib/systemd/system/tftp.service /etc/systemd/system
+mv /etc/systemd/system/tftp.service /etc/systemd/system/tftp.service.org
+yes |cp -fra /usr/src/PXE-Server-Centos-7/conf/tftp.service /etc/systemd/system/tftp.service
+
+
+-------------------- OPERATING SYSTEMS ------------------
+
+### CREATE MIRROR LOCAL - CENTOS-7
+
+VERSION="CentOS-7-x86_64-Minimal-1810.iso";
+URL="http://mirrors.oit.uci.edu/centos/7.6.1810/isos/x86_64"
+CHECKOUT_DIR="/usr/src/isobuild";
+mkdir -p  ${CHECKOUT_DIR}
+mkdir -p /var/www/html/repos/centos/7/{os/x86_64,updates/x86_64}
+cd /usr/src && wget ${URL}/${VERSION}
+mount -t iso9660 -o loop /usr/src/${VERSION} ${CHECKOUT_DIR}
+rsync -a -H  ${CHECKOUT_DIR}/ /var/www/html/repos/centos/7/os/x86_64/
+umount ${CHECKOUT_DIR}
+
+
+
+### CREATE MIRROR ISSABEL - CENTOS-7 + ISSABEL/ASTERISK PBX SYSTEM
+
+VERSION="Issabel4-USB-DVD-x86_64-20200102.iso";
+URL="http://mirrors.oit.uci.edu/centos/7.6.1810/isos/x86_64";
+CHECKOUT_DIR="/usr/src/isobuild";
+mkdir -p  ${CHECKOUT_DIR}
+mkdir -p /var/www/html/repos/issabel/4/{os/x86_64,updates/x86_64}
+cd /usr/src && wget ${URL}/${VERSION}
+mount -t iso9660 -o loop /usr/src/${VERSION} ${CHECKOUT_DIR}
+rsync -a -H  ${CHECKOUT_DIR}/ /var/www/html/repos/centos/7/os/x86_64/
+umount ${CHECKOUT_DIR}
+
+
+-------------------- CONFIGURATIONS ------------------
+
+### NGINX
+
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.org
+yes |cp -fra /usr/src/PXE-Server-Centos-7/nginx/* /etc/nginx/
+
+
+
+### KICK START CONFIG
+
+mkdir -p /var/www/html/repos/ks
+yes |cp -fra /usr/src/PXE-Server-Centos-7/ks/* /var/www/html/repos/ks/
+
+
+
+### PROJECT GIT
+
+#cd /usr/src/
+#git clone https://github.com/obe711/PXE-Server-Centos-7.git
+
+
+
+### INSTALL PACKAGES
+
+yum -y install vim wget net-tools mc screen openssl rsync git
+yum -y install syslinux tftp-server
+systemctl start tftp.socket
+systemctl enable tftp.socket
+#journalctl -f -n0
---
+++ README.md
@@ -0,0 +1,19 @@
+# PXE-Server-Centos-7
+
+This code is distributed under the GNU LGPL v3.0 license.
+
+## Introduction
+
+## Installation
+
+Install the git package and follow the instructions on a [CentOS 7 64bit](https://wiki.centos.org/Documentation)
+
+```bash
+bash <( curl -s https://raw.githubusercontent.com/obe711/PXE-Server-Centos-7/master/pxe-server_installer.sh )
+```
+
+## Password
+
+The user is: root
+
+The password is: sigconsultingservices.com
