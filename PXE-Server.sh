4525ef1c12a435b70d11d0d2faf5fdf64f231586
obe711 <obe711@gmail.com>
2020-05-20 00:20:47 -0400

    c

--- PXE-Server.sh
+++ PXE-Server.sh
@@ -18,10 +18,10 @@
 
 ln -s '/usr/lib/systemd/system/tftp.socket' '/etc/systemd/system/sockets.target.wants/tftp.socket'
 cd /var/lib/tftpboot
-yes |cp -fra /usr/src/PXE-Server-Centos-7/isolinux/* /var/lib/tftpboot/
+yes |cp -fra /usr/src/PXE-Server/isolinux/* /var/lib/tftpboot/
 cp /usr/lib/systemd/system/tftp.service /etc/systemd/system
 mv /etc/systemd/system/tftp.service /etc/systemd/system/tftp.service.org
-yes |cp -fra /usr/src/PXE-Server-Centos-7/conf/tftp.service /etc/systemd/system/tftp.service
+yes |cp -fra /usr/src/PXE-Server/conf/tftp.service /etc/systemd/system/tftp.service
 
 
 -------------------- OPERATING SYSTEMS ------------------
@@ -49,7 +49,7 @@ mkdir -p  ${CHECKOUT_DIR}
 mkdir -p /var/www/html/repos/issabel/4/{os/x86_64,updates/x86_64}
 cd /usr/src && wget ${URL}/${VERSION}
 mount -t iso9660 -o loop /usr/src/${VERSION} ${CHECKOUT_DIR}
-rsync -a -H  ${CHECKOUT_DIR}/ /var/www/html/repos/centos/7/os/x86_64/
+rsync -a -H  ${CHECKOUT_DIR}/ /var/www/html/repos/issabel/4/os/x86_64/
 umount ${CHECKOUT_DIR}
 
 
@@ -58,28 +58,19 @@ umount ${CHECKOUT_DIR}
 ### NGINX
 
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.org
-yes |cp -fra /usr/src/PXE-Server-Centos-7/nginx/* /etc/nginx/
+yes |cp -fra /usr/src/PXE-Server/nginx/* /etc/nginx/
 
 
 
 ### KICK START CONFIG
 
 mkdir -p /var/www/html/repos/ks
-yes |cp -fra /usr/src/PXE-Server-Centos-7/ks/* /var/www/html/repos/ks/
+yes |cp -fra /usr/src/PXE-Server/ks/* /var/www/html/repos/ks/
 
 
 
 ### PROJECT GIT
 
 #cd /usr/src/
-#git clone https://github.com/obe711/PXE-Server-Centos-7.git
+#git clone https://github.com/obe711/PXE-Server.git
 
-
-
-### INSTALL PACKAGES
-
-yum -y install vim wget net-tools mc screen openssl rsync git
-yum -y install syslinux tftp-server
-systemctl start tftp.socket
-systemctl enable tftp.socket
-#journalctl -f -n0
